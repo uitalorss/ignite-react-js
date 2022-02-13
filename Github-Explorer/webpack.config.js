94fad76f3269d5e8a9882319be2b9fb9da37d851
Uítalo <uitalo.urss@gmail.com>
2022-02-13 01:54:20 -0300

    configurando o source map e criando ambientes de desenvolvimento e produção

--- Github-Explorer/webpack.config.js
+++ Github-Explorer/webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -10,6 +12,11 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public')
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
