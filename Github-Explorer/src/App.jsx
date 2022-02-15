f120bc3e09f2511bedf88140ea945a6560e12978
Uítalo <uitalo.urss@gmail.com>
2022-02-15 00:09:48 -0300

    configurando o sass

--- Github-Explorer/src/App.jsx
+++ Github-Explorer/src/App.jsx
@@ -1,6 +1,6 @@
 import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList'
-import './styles/style.css'
+import './styles/style.scss'
 
 export function App(){
   return (
