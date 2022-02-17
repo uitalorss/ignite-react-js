fad3d8f334da6e2a6b572765559b1ff9710c55ac
Uítalo <uitalo.urss@gmail.com>
2022-02-17 00:41:07 -0300

    pegando os dados do meu repositório para serem exibidos

--- Github-Explorer/src/components/RepositoryList.jsx
+++ Github-Explorer/src/components/RepositoryList.jsx
@@ -1,5 +1,8 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss'
+import { useEffect, useState } from "react";
+
+//https://api.github.com/users/uitalorss/repos
 
 const repository = {
   name: "unform",
@@ -8,6 +11,16 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState(); //IMPORTANTE: Sempre começar o estado com uma variável com o mesmo tipo de dado que eu quero armazenar.
+
+  useEffect(() =>{
+    fetch('https://api.github.com/users/uitalorss/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+  }, [])
+
+  console.log(repositories);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
